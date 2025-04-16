0b6d3a1b41b3e8db719cc25f0783600afb53c82e
Gloria Molina Pico <gmpanalisis@gmail.com>
2025-04-16 16:32:12 -0300

    feature engineering

---
+++ FE_gmp.py
@@ -0,0 +1,17 @@
+# Importación de Librerías
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns 
+from wordcloud import WordCloud
+import os
+import re
+
+
+
+
+
+
+%matplotlib inline
+pd.set_option('display.max_columns', 30)
+plt.rcParams['figure.figsize'] = [12.0, 8.0]
